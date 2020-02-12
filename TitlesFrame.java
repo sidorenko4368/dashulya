a81f7872fb977abaea08224067687a0b9fc73739
sidorenko4368 <56118322+sidorenko4368@users.noreply.github.com>
2020-02-12 18:47:03 +0200

    Update TitlesFrame.java

--- TitlesFrame.java
+++ TitlesFrame.java
@@ -5,20 +5,13 @@ import java.awt.Component;
 import javax.swing.JFrame;
 
 
-/**
- * 
- * This class extends JFrame
- *
- */
+
 public class TitlesFrame extends JFrame
 {
     public TitlesFrame() {
         this.initUI();
     }
-    /**
-     * 
-     * @deprecated initUI Here is we can set size and form. To change my program I set value of titlesPanel(97)
-     */
+
     private void initUI() {
         this.setTitle("\u041a\u0440\u0438\u0432\u044b\u0435 \u0444\u0438\u0433\u0443\u0440\u044b");
         this.setDefaultCloseOperation(3);
@@ -29,12 +22,9 @@ public class TitlesFrame extends JFrame
 
     public static void main(final String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
-            @Override
+
             
-            /**
-             * 
-             * @deprecated run set our object visible
-             */
+
             public void run() {
                 final TitlesFrame ps = new TitlesFrame();
                 ps.setVisible(true);
